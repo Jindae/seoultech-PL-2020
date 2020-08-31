af5896f46382bd034b54f0b4033ab40c07acf762
Jindae <jindae.kim@seoultech.ac.kr>
2020-08-31 09:46:26 +0900

    Materials for the first week.

---
+++ practices/practice1/helloworld.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+/*
+    This is a main function,
+    which the entry point of a program.
+*/
+int main() {
+    cout << "Hello World!!\n";    
+    return 0;
+}
